07f410b1b9b60e4ed9e9bb20884856cd5ae6c830
tarun sinha <tarunsinha.m2009@gmail.com>
2021-05-29 00:42:51 +0530

    Single number using XOR

--- LeetCode/SingleInteger.cs
+++ LeetCode/SingleInteger.cs
@@ -12,4 +12,18 @@ public class Solution
         }
         return n[0];
     }
+
+    public int SingleNumberUsingXOR(int [] nums){
+        // Any number XOR with 0 is that number
+        // Any number XOR with itself is 0
+        // Therefore [5,4,1,4,3,5,1]
+        // XOR each number with the other one gives 0 when two numbers are same
+        // Last number would be the remaining number which is the number with single occurence
+        int a =0;
+        foreach (int i in num)
+        {
+            a = a^i;
+        }
+        return a;
+    }
 }
